d250b289f8f15d7e018b8659b5b1fa196653a1b6
Julio Melo <juliocsmelo@gmail.com>
2015-05-15 22:38:51 -0300

    mais dados no chart

--- dist/js/charts/lines.js
+++ dist/js/charts/lines.js
@@ -1,16 +1,40 @@
 var ctx = document.getElementById("chart_sales_month_line").getContext("2d")
 var data = {
-    labels: ["01", "02", "03", "04", "05", "06", "07"],
+    labels: ["01", 
+    "02", 
+    "03", 
+    "04", 
+    "05", 
+    "06", 
+    "07", 
+    "08", 
+    "09", 
+    "10", 
+    "11", 
+    "12", 
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    ],
     datasets: [
         {
             label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 56, 55, 40,]
         },
     ]
 };
