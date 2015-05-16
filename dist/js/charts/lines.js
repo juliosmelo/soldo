943940dfef12b2fb45e9faf5f8a927e216b26610
Julio Melo <juliocsmelo@gmail.com>
2015-05-15 22:55:35 -0300

    highchts line

--- dist/js/charts/lines.js
+++ dist/js/charts/lines.js
@@ -4,7 +4,7 @@ $(function () {
             type: 'line'
         },
         title: {
-            text: 'Vendas por dia'
+            text: 'Vendas por dia Maio 2015'
         },
         xAxis: {
             categories: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
